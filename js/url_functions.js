aed56fb69b0447772db7503d75bfcf8af6942454
tanda <tanda@kankan.com>
2016-11-08 19:17:57 +0800

    更新

--- js/url_functions.js
+++ js/url_functions.js
@@ -31,3 +31,13 @@ function parseURL(url) {
         segments: a.pathname.replace(/^\//,'').split('/')
     };
 }
+
+
+var getUrlParam = function (name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) {
+        return decodeURI(r[2]);
+    }
+    return null;
+};
--- php/calculate_functions.php
+++ php/calculate_functions.php
@@ -58,4 +58,13 @@ function getDistanceBetweenPointsNew ($latitude1, $longitude1, $latitude2, $long
     return compact('miles', 'feet', 'yards', 'kilometers', 'meters');
 }
 
+/**
+ * 返回当前微妙级别系统时间
+ * Simple function to replicate PHP 5 behaviour
+ */
+function microtime_float()
+{
+    list($usec, $sec) = explode(" ", microtime());
+    return ((float)$usec + (float)$sec);
+}
 
